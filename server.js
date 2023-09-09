4aa54be3af4ea97067b12f7a3e59cd32f7435566
Techinfo YT <49879287+techinfo-youtube@users.noreply.github.com>
2023-09-10 01:43:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/v1/post", require("./routes/postRoutes"));
 //home
 app.get("/", (req,res)=>{
   res.status(200).send({
-    ""success:true,
+    "success":true,
     "msg":"Node Serevr Running"
   })
 })
